bd3b11307f5f627d28699d1570e819f172eba99a
Ronaldo S.A. Batista <ronaldokun@gmail.com>
2025-05-01 14:12:41 -0300

    📌 Pinned nginx version

--- 01-primeiro-container/run_containers.sh
+++ 01-primeiro-container/run_containers.sh
@@ -1,5 +1,5 @@
 docker pull nginx:latest
-docker run -d -p 8080:80 --name "meu-servidor" nginx:latest
+docker run -d -p 8080:80 --name "meu-servidor" nginx:1.27.5-bookworm
 docker ps
 docker container stop "meu-servidor"
 docker container rm "meu-servidor"
